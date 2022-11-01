47a1bd37653fbe6bbee2dfbbba273f93511dd9b2
alan hernandez <felipecoderhouse@gmail.com>
2022-10-31 22:42:50 -0300

    trabajo js

--- index.js
+++ index.js
@@ -1,82 +1,185 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Variables
+    const baseDeDatos = [
+        {
+            id: 1,
+            nombre: 'Fiat',
+            precio: 1500000,
+            imagen: 'auto.jpg'
+        },
+        {
+            id: 2,
+            nombre: 'Chevrolet',
+            precio: 2500000,
+            imagen: 'auto2.jpg'
+        },
+        {
+            id: 3,
+            nombre: 'Chevrolet',
+            precio: 2700000,
+            imagen: 'auto3.jpg'
+        },
+        {
+            id: 4,
+            nombre: 'Peugeot',
+            precio: 3000000,
+            imagen: 'auto4.jpg'
+        },
+        {
+            id: 5,
+            nombre: 'Ford',
+            precio: 3600000,
+            imagen: 'auto5.jpg'
+        },
+        {
+            id: 6,
+            nombre: 'Citroen',
+            precio: 5000000,
+            imagen: 'auto6.jpg'
+        }
+    ];
 
-// datos de usuario
-const user = "hernandez"
-const pass_cliente = "alan123"
-//
-function solicitarDatos (){
-    let usuario = prompt("ingrese su usuario");
-    let password = prompt ("ingrese su password")
+    let carrito = [];
+    const divisa = '$';
+    const DOMitems = document.querySelector('#items');
+    const DOMcarrito = document.querySelector('#carrito');
+    const DOMtotal = document.querySelector('#total');
+    const DOMbotonVaciar = document.querySelector('#boton-vaciar');
 
-if (validarDatos (usuario,password)){
-    alert("bienvenidos a autos hernandez ")
+    // Funciones
+
+    /**
     
-}else{
-    alert("usuario y o password incorrecto")
-} 
-
-}
- function validarDatos(usuario,password){
-    if ( usuario === user && password === pass_cliente){
-        return true;
-    } else {
-        return false
-       
+    */
+    function renderizarProductos() {
+        baseDeDatos.forEach((info) => {
+            // Estructura
+            const miNodo = document.createElement('div');
+            miNodo.classList.add('card', 'col-sm-4');
+            // Body
+            const miNodoCardBody = document.createElement('div');
+            miNodoCardBody.classList.add('card-body');
+            // Titulo
+            const miNodoTitle = document.createElement('h5');
+            miNodoTitle.classList.add('card-title');
+            miNodoTitle.textContent = info.nombre;
+            // Imagen
+            const miNodoImagen = document.createElement('img');
+            miNodoImagen.classList.add('img-fluid');
+            miNodoImagen.setAttribute('src', info.imagen);
+            // Precio
+            const miNodoPrecio = document.createElement('p');
+            miNodoPrecio.classList.add('card-text');
+            miNodoPrecio.textContent = `${info.precio}${divisa}`;
+            // Boton 
+            const miNodoBoton = document.createElement('button');
+            miNodoBoton.classList.add('btn', 'btn-primary');
+            miNodoBoton.textContent = 'Agregar';
+            miNodoBoton.setAttribute('marcador', info.id);
+            miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+            // Insertamos
+            miNodoCardBody.appendChild(miNodoImagen);
+            miNodoCardBody.appendChild(miNodoTitle);
+            miNodoCardBody.appendChild(miNodoPrecio);
+            miNodoCardBody.appendChild(miNodoBoton);
+            miNodo.appendChild(miNodoCardBody);
+            DOMitems.appendChild(miNodo);
+        });
     }
- } 
-  solicitarDatos()
 
- // variables declaradas según el tipo de auto.
- let vPe = 0;
- let vMed = 0;
- let vGrand = 0;
- let vTotal = 0;
-// variables declaradas según el precio del auto.
- let vPe1 = 500;
- let vMed2 = 1000;
- let vGrand3 = 1500;
-// variables declaradas según el acumulado de $.
- let vPequeAcu = 0;
- let vMeDAcu = 0;
- let vGrandAcu = 0;
- let vTotalAcu = 0;
- let promedio = 0;
-// variable para el usuario ingresara el tipo del auto
- let au;
-//
-let cant = 1;
-while (cant <= 5) 
-{
- let ingreso = false; 
- while (ingreso == false) 
- {
-     au = parseInt(prompt("ingrese tipo de auto:\n 1)= pequeño\n 2)= mediano\n 3)= grande"));
-     if (au <= 3) {
-         ingreso = true;
-     }
-     else{
-         alert(" no has ingresado un valor correcto. Volve a intentarlo. ")
-     }
- }
- switch (au)
- {
-     case 1:
-         vPe++;
-         vPequeAcu = vPequeAcu + vPe1
-         break;
-     case 2:
-         vMed++;
-         vMeDAcu = vMeDAcu + vMed2
-         break;
-     case 3:
-         vGrand++;
-         vGrandAcu = vGrandAcu + vGrand3
-         break;
- }
- vTotal = vPe + vMed + vGrand;
- vTotalAcu = vPequeAcu + vMeDAcu + vGrandAcu;
- alert("cantidad de autos ingresados por el momento: " + vTotal + "\n recaudado: " +"$"+ vTotalAcu);
- cant++;
-}
-//promedio = vTotalAcu / vTotal;
-//alert ( "valor promedio registrado por auto es equivalente a pesos: "  +  promedio ) ;
+    /**
+    * Evento para añadir un producto al carrito de la compra
+    */
+    function anyadirProductoAlCarrito(evento) {
+        // Anyadimos el Nodo al carrito
+        carrito.push(evento.target.getAttribute('marcador'))
+        // Actualizamos el carrito 
+        renderizarCarrito();
 
+    }
+
+    /**
+    * Dibuja todos los productos guardados en el carrito
+    */
+    function renderizarCarrito() {
+        // Vaciamos todo el html
+        DOMcarrito.textContent = '';
+        // Quitamos los duplicados
+        const carritoSinDuplicados = [...new Set(carrito)];
+        // Generamos los Nodos a partir de carrito
+        carritoSinDuplicados.forEach((item) => {
+            // Obtenemos el item que necesitamos de la variable base de datos
+            const miItem = baseDeDatos.filter((itemBaseDatos) => {
+                // ¿Coincide las id? Solo puede existir un caso
+                return itemBaseDatos.id === parseInt(item);
+            });
+            // Cuenta el número de veces que se repite el producto
+            const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+                // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
+                return itemId === item ? total += 1 : total;
+            }, 0);
+            // Creamos el nodo del item del carrito
+            const miNodo = document.createElement('li');
+            miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+            miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
+            // Boton de borrar
+            const miBoton = document.createElement('button');
+            miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+            miBoton.textContent = 'X';
+            miBoton.style.marginLeft = '1rem';
+            miBoton.dataset.item = item;
+            miBoton.addEventListener('click', borrarItemCarrito);
+            // Mezclamos nodos
+            miNodo.appendChild(miBoton);
+            DOMcarrito.appendChild(miNodo);
+        });
+       // Renderizamos el precio total en el HTML
+       DOMtotal.textContent = calcularTotal();
+    }
+
+    /**
+    * Evento para borrar un elemento del carrito
+    */
+    function borrarItemCarrito(evento) {
+        // Obtenemos el producto ID que hay en el boton pulsado
+        const id = evento.target.dataset.item;
+        // Borramos todos los productos
+        carrito = carrito.filter((carritoId) => {
+            return carritoId !== id;
+        });
+        // volvemos a renderizar
+        renderizarCarrito();
+    }
+
+    /**
+     * Calcula el precio total teniendo en cuenta los productos repetidos
+     */
+    function calcularTotal() {
+        // Recorremos el array del carrito 
+        return carrito.reduce((total, item) => {
+            // De cada elemento obtenemos su precio
+            const miItem = baseDeDatos.filter((itemBaseDatos) => {
+                return itemBaseDatos.id === parseInt(item);
+            });
+            // Los sumamos al total
+            return total + miItem[0].precio;
+        }, 0).toFixed(2);
+    }
+
+    /**
+    * Varia el carrito y vuelve a dibujarlo
+    */
+    function vaciarCarrito() {
+        // Limpiamos los productos guardados
+        carrito = [];
+        // Renderizamos los cambios
+        renderizarCarrito();
+    }
+
+    // Eventos
+    DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+
+    // Inicio
+    renderizarProductos();
+    renderizarCarrito();
+  });
